e152b045b948223285826712cede11f55eaa3549
David Silaban <david.silaban@gmail.com>
2021-08-23 20:40:38 +0700

    TMDB API interface

---
+++ TheMovieDB/TMDBAPI/APIHTTPRequest.swift
@@ -0,0 +1,50 @@
+//
+//  APIHTTPRequest.swift
+//  TheMovieDB
+//
+//  Created by David Silaban on 23/08/21.
+//
+
+import Foundation
+
+class APIHTTPRequest {
+    private let API_KEY = "02bdafe8119422f52cae64bfff62b3be"
+    private let BASE_URL = "https://api.themoviedb.org/3"
+    
+    private func httpRequest(method: String, endpoint: String, params: [String:Any]?, jsonBody: [String:Any]?, completionHandler: @escaping (_ success: Bool, _ errorCode: Int, _ response: [String:Any]?) -> Void) {
+        var urlComponents = URLComponents(string: BASE_URL+endpoint)
+        var theParams = ["api_key": API_KEY as Any]
+        if let pars = params {
+            theParams.merge(pars) { (current, _) in current }
+        }
+        urlComponents?.queryItems = theParams.map { (key, value) in
+            URLQueryItem(name: key, value: String(describing: value))
+        }
+        if let url = urlComponents?.url {
+            var request = URLRequest(url: url)
+            request.httpMethod = method
+            if let body = jsonBody {
+                request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                let httpResponse = response as? HTTPURLResponse
+                if let responseData = data {
+                    let jsonObject = try? JSONSerialization.jsonObject(with: responseData, options: [])
+                    completionHandler(error == nil, httpResponse?.statusCode ?? 0, jsonObject as? [String:Any])
+                } else {
+                    completionHandler(error == nil, httpResponse?.statusCode ?? 0, nil)
+                }
+            }.resume()
+        }
+    }
+    
+    func httpGet(endpoint: String, params: [String:Any]?, completionHandler: @escaping (_ success: Bool, _ errorCode: Int, _ response: [String:Any]?) -> Void) {
+        httpRequest(method: "GET", endpoint: endpoint, params: params, jsonBody: nil, completionHandler: completionHandler)
+    }
+    
+    func httpPost(endpoint: String, params: [String:Any]?, jsonBody: [String:Any], completionHandler: @escaping (_ success: Bool, _ errorCode: Int, _ response: [String:Any]?) -> Void) {
+        httpRequest(method: "POST", endpoint: endpoint, params: params, jsonBody: jsonBody, completionHandler: completionHandler)
+    }
+}
---
+++ TheMovieDB/TMDBAPI/Interface.swift
@@ -0,0 +1,34 @@
+//
+//  Interface.swift
+//  TheMovieDB
+//
+//  Created by David Silaban on 23/08/21.
+//
+
+import Foundation
+
+class TMDBAPIInterface {
+    static let shared = TMDBAPIInterface()
+    
+    private let apiHttpRequest = APIHTTPRequest()
+    
+    func getMovieGenres(completionHandler: @escaping (Bool, Int, [String:Any]?) -> Void) {
+        apiHttpRequest.httpGet(endpoint: "/genre/movie/list", params: nil, completionHandler: completionHandler)
+    }
+    
+    func getDiscoverMovies(genreId: Int, page: Int, completionHandler: @escaping (Bool, Int, [String:Any]?) -> Void) {
+        apiHttpRequest.httpGet(endpoint: "/discover/movie", params: ["with_genres": genreId, "page": page, "include_video": true], completionHandler: completionHandler)
+    }
+    
+    func getPrimaryInfoByMovieId(movieId: Int, completionHandler: @escaping (Bool, Int, [String:Any]?) -> Void) {
+        apiHttpRequest.httpGet(endpoint: "/movie/\(movieId)", params: nil, completionHandler: completionHandler)
+    }
+    
+    func getVideosByMovieId(movieId: Int, completionHandler: @escaping (Bool, Int, [String:Any]?) -> Void) {
+        apiHttpRequest.httpGet(endpoint: "/movie/\(movieId)/videos", params: nil, completionHandler: completionHandler)
+    }
+    
+    func getReviewsByMovieId(movieId: Int, page: Int, completionHandler: @escaping (Bool, Int, [String:Any]?) -> Void) {
+        apiHttpRequest.httpGet(endpoint: "/movie/\(movieId)/reviews", params: ["page": page], completionHandler: completionHandler)
+    }
+}
