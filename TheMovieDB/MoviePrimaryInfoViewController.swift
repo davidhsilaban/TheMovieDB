89ad2f0193f3cf73bd582eb8b596974ac38a9863
David Silaban <david.silaban@gmail.com>
2021-08-23 23:03:52 +0700

    Refactor movie primary info

--- TheMovieDB/MoviePrimaryInfoViewController.swift
+++ TheMovieDB/MoviePrimaryInfoViewController.swift
@@ -10,8 +10,13 @@ import MBProgressHUD
 
 class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var movieId: Int?
+    private var movieDataDict: [String:Any]?
     private var movieDataKeys: [String] = []
     private var movieDataValues: [String] = []
+    private var movieDataGenres: [String] = []
+    private var movieDataCompanies: [String] = []
+    private var movieDataCountries: [String] = []
+    private var movieDataLanguages: [String] = []
     private var movieVideos: [[String:Any]]?
     
     @IBOutlet weak var imageView: UIImageView!
@@ -36,7 +41,7 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
                 
                 DispatchQueue.global().async { [weak self] in
                     if let posterPath = data?["poster_path"] as? String {
-                        var urlComponents = URLComponents(string: "https://image.tmdb.org/t/p/original"+posterPath)
+                        let urlComponents = URLComponents(string: "https://image.tmdb.org/t/p/original"+posterPath)
 //                        var theParams = ["api_key": APIHTTPRequest.API_KEY as Any]
 //                        urlComponents?.queryItems = theParams.map { (key, value) in
 //                            URLQueryItem(name: key, value: String(describing: value))
@@ -55,9 +60,38 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
                 
                 DispatchQueue.main.async {
                     if let res = data {
+                        self.movieDataDict = res
                         for (key, value) in Array(res).sorted(by: {$0.0 < $1.0}) {
                             self.movieDataKeys.append(key)
                             self.movieDataValues.append(String(describing: value))
+                        }
+                        
+                        // Genres
+                        if let genres = res["genres"] as? [[String:Any]] {
+                            for genre in genres {
+                                self.movieDataGenres.append(genre["name"] as! String)
+                            }
+                        }
+                        
+                        // Companies
+                        if let companies = res["production_companies"] as? [[String:Any]] {
+                            for company in companies {
+                                self.movieDataCompanies.append(company["name"] as! String)
+                            }
+                        }
+                        
+                        // Countries
+                        if let countries = res["production_countries"] as? [[String:Any]] {
+                            for country in countries {
+                                self.movieDataCountries.append(country["name"] as! String)
+                            }
+                        }
+                        
+                        // Languages
+                        if let languages = res["spoken_languages"] as? [[String:Any]] {
+                            for language in languages {
+                                self.movieDataLanguages.append(language["name"] as! String)
+                            }
                         }
                     }
                     self.tableView.reloadData()
@@ -71,19 +105,37 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
 
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 9
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section {
         case 0:
-            return movieDataKeys.count
+            return 1
             
         case 1:
-            return movieVideos?.count ?? 0
+            return 1
             
         case 2:
+            return movieDataKeys.count
+            
+        case 3:
+            return movieVideos?.count ?? 0
+            
+        case 4:
+            return movieDataGenres.count
+            
+        case 5:
+            return movieDataCompanies.count
+            
+        case 6:
+            return movieDataCountries.count
+            
+        case 7:
+            return movieDataLanguages.count
+            
+        case 8:
             return 1
             
         default:
@@ -97,14 +149,42 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
         // Configure the cell...
         switch indexPath.section {
         case 0:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataDict?["tagline"] as? String
+            
+        case 1:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataDict?["overview"] as? String
+            
+        case 2:
             cell.textLabel?.text = movieDataKeys[indexPath.row]
             cell.detailTextLabel?.text = movieDataValues[indexPath.row]
             
-        case 1:
+        case 3:
             cell = tableView.dequeueReusableCell(withIdentifier: "youtubeCell", for: indexPath)
             break
             
-        case 2:
+        case 4:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataGenres[indexPath.row]
+            break
+            
+        case 5:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataCompanies[indexPath.row]
+            break
+            
+        case 6:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataCountries[indexPath.row]
+            break
+            
+        case 7:
+            cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = movieDataLanguages[indexPath.row]
+            break
+            
+        case 8:
             cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell", for: indexPath)
             break
             
@@ -119,12 +199,30 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0:
-            return "Primary Info"
+            return "Tagline"
             
         case 1:
-            return "Videos"
+            return "Overview"
             
         case 2:
+            return "Primary Info"
+            
+        case 3:
+            return "Videos"
+            
+        case 4:
+            return "Genres"
+            
+        case 5:
+            return "Production Companies"
+            
+        case 6:
+            return "Production Countries"
+            
+        case 7:
+            return "Spoken Languages"
+            
+        case 8:
             return "Reviews"
             
         default:
@@ -133,7 +231,7 @@ class MoviePrimaryInfoViewController: UIViewController, UITableViewDelegate, UIT
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 {
+        if indexPath.section == 3 {
             let videoData = movieVideos?[indexPath.row]
             let videoKey = videoData?["key"] as? String ?? ""
             if let url = URL(string: "https://youtu.be/"+videoKey) {
